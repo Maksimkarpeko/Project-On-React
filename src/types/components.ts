45247acd06bf3e8547b1252d983f6230c98d15f4
Maksim Karpeko <maksim_karpe2ko@mail.ru>
2025-06-21 14:20:21 +0300

    feat:update

--- src/types/components.ts
+++ src/types/components.ts
@@ -1,7 +1,7 @@
 import type { ButtonHTMLAttributes } from 'react';
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
-  loading:boolean;
+  loading?:boolean;
   text: string;
   type: 'button' | 'submit' | 'reset';
   img?: string;
