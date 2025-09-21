17ace701990a8a35c34f0c57ced81e0cffc8f6ac
Maksim <maksim_karpe2ko@mail.ru>
2025-09-21 13:21:25 +0300

    fix:-

--- Front-end/src/components/StartScreen/StartScreen.tsx
+++ Front-end/src/components/StartScreen/StartScreen.tsx
@@ -1,7 +1,7 @@
 import { type FC, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { checkAuth } from 'api/auth1';
+import { checkAuth } from 'api/auth';
 import { LogoXL } from 'assets/index';
 import { Button } from 'components/common/Button/Button';
 import { buttonSize } from 'components/common/Button/constant';
--- Front-end/src/pages/SignUp/SignUp.tsx
+++ Front-end/src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { signUp } from 'api/auth1';
+import { signUp } from 'api/auth';
 import { AuthEntry } from 'components/AuthEntry/AuthEntry';
 import { ErrorMessage } from 'components/common/ErrorMessage/ErrorMessage';
 import { Input } from 'components/common/Input/Input';
